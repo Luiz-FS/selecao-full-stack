7bc0d294434ee0fa7f9cb431ca1b257dfbe90de9
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-18 15:20:09 -0300

    Created  Coin Model

--- backend/realtimequote/coin/models.py
+++ backend/realtimequote/coin/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from realtimequote.base_model import BaseModel
 
-# Create your models here.
+
+
+class Coin(BaseModel):
+    name = models.CharField(max_length=30, unique=True, null=False, blank=False)
+    description = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=10 ,decimal_places=2, null=False, blank=False)
