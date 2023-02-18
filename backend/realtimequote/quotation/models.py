9f0e52de5f4d11e0e27042368f138d96f48a4e02
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-18 15:21:27 -0300

    Created Quotation model

--- backend/realtimequote/quotation/models.py
+++ backend/realtimequote/quotation/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from realtimequote.base_model import BaseModel
+from coin.models import Coin
 
-# Create your models here.
+class Quotation(BaseModel):
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
+    min_price = models.DecimalField(max_digits=8, decimal_places=4, null=False, blank=False)
+    max_price = models.DecimalField(max_digits=8, decimal_places=4, null=False, blank=False)
+    variance = models.IntegerField(null=False, blank=False)
+    create_date = models.DateField(null=False, blank=False)
