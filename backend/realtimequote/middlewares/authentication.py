16aab08e2ab1e46f9a35d4bc4b2db0f42bb3fa52
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-18 15:24:28 -0300

    Created Oauth authenticator

---
+++ backend/realtimequote/middlewares/authentication.py
@@ -0,0 +1,47 @@
+import requests
+from http import HTTPStatus
+from django.contrib.auth.models import User
+from rest_framework import authentication
+from rest_framework import exceptions
+from drf_spectacular.extensions import OpenApiAuthenticationExtension
+from realtimequote import settings
+
+
+class JWTAuthentication(authentication.BaseAuthentication):
+
+    def authenticate_header(self, request):
+        return HTTPStatus.UNAUTHORIZED
+
+    def authenticate(self, request):
+        token = request.headers.get("Authorization")
+
+        if token != None and request.session.get("token") == token:
+            data = request.session.get("user")
+            return (User(id=data["id"], email=data["email"]), token)
+        elif not token:
+            return None
+
+        response = requests.get(f"{settings.AUTHENTICATOR_URI}/auth/verify?token={token}")
+        
+        if response.status_code != HTTPStatus.OK:
+            raise exceptions.AuthenticationFailed("Invalid auth token!")
+        
+        data = response.json()
+
+        request.session["user"] = data
+        request.session["token"] = token
+
+        user = User(id=data["id"], email=data["email"])
+        return (user, token)
+
+
+class JWTAuthenticationScheme(OpenApiAuthenticationExtension):  # pragma nocover
+    target_class = 'middlewares.authentication.JWTAuthentication'
+    name = 'JWTAuth'
+
+    def get_security_definition(self, auto_schema):
+        return {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization',
+        }
