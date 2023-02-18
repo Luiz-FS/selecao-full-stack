76246a77e81adc7d338ffdd8f01e680543f8cddd
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-18 15:24:00 -0300

    Setup projet

--- backend/realtimequote/realtimequote/settings.py
+++ backend/realtimequote/realtimequote/settings.py
@@ -11,9 +11,35 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+from urllib.parse import urlparse
+from kombu import Exchange, Queue
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DB_URI = config(
+    "DB_URI", default="postgresql://postgres:postgres@localhost/tradex", cast=str
+)
+AUTHENTICATOR_URI = config(
+    "AUTHENTICATOR_URI", default="http://localhost:8001", cast=str
+)
+uri_params = urlparse(DB_URI)
+DB_HOST = uri_params.hostname
+DB_USER = uri_params.username
+DB_PASS = uri_params.password
+DB_NAME = uri_params.path.strip("/")
+BROKER_URL = config("BROKER_URL", default="amqp://root:secret@localhost:5672//", cast=str)
+ENVIRONMENT = config("SIMPLE_SETTINGS", "realtimequote.realtimequote.settings").split(".")[-1:][0]
+
+
+# Django timezones and languages settings
+# https://docs.djangoproject.com/en/4.1/ref/settings
+TIME_ZONE = "UTC"
+USE_TZ = True
+USE_I18N = False
+LANGUAGE_CODE = "en"
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +51,13 @@ SECRET_KEY = 'django-insecure-u)^c2w8nhc*ulj4ih1t0)4(np^_s(0mo*ly23b1pzqei4396^k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10*1024*1024  # 10MB
+CORS_ORIGIN_ALLOW_ALL = True
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+SESSION_COOKIE_AGE = 60 * 60
 
 
 # Application definition
@@ -39,6 +71,16 @@ DEFAULT_APPS = [
     'django.contrib.staticfiles',
 ]
 
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "rest_framework.authtoken",
+    "drf_spectacular",
+    "django_filters",
+    "celery",
+    "django_celery_beat",
+    "django_celery_results"
+]
+
 
 LOCAL_APPS = [
     'coin.apps.CoinConfig',
@@ -46,8 +88,23 @@ LOCAL_APPS = [
 ]
 
 
-INSTALLED_APPS = DEFAULT_APPS + LOCAL_APPS
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "middlewares.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'middlewares.pagination.CustomPagination',
+    'PAGE_SIZE': 10,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,6 +114,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'realtimequote.urls'
@@ -84,9 +142,13 @@ WSGI_APPLICATION = 'realtimequote.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "OPTIONS": {"options": "-c search_path=public"},
+        "HOST": DB_HOST,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASS,
+        "NAME": DB_NAME,
     }
 }
 
@@ -109,6 +171,40 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Celery Configuration Global
+# https://docs.celeryproject.org/en/latest/userguide/configuration.html
+CELERY_BROKER_URL = BROKER_URL
+CELERY_WORKERS = config("CELERY_WORKERS", default=1, cast=int)
+CELERY_TIME_ZONE = TIME_ZONE
+CELERY_ENABLE_UTC = True
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TASK_SERIALIZER = "json"
+CELERY_CONTENT_ENCODING = "utf-8"
+CELERY_ALWAYS_EAGER = False
+CELERY_WORKER_MAX_TASKS_PER_CHILD = 3
+
+# Celery Configuration "Django Celery Beat"
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
+# Celery Configuration "Celery Result Backend"
+CELERY_RESULT_BACKEND = "django-db"
+DJANGO_CELERY_RESULTS_TASK_ID_MAX_LENGTH = 191
+
+# Celery Configuration "Flower"
+CELERY_WORKER_ENABLE_REMOTE_CONTROL = False
+CELERY_WORKER_SEND_TASK_EVENTS = False
+CELERY_FLOWER_BASIC_AUTH = config("CELERY_FLOWER_BASIC_AUTH", default="root:secret", cast=str)
+
+# Celery Task Queues
+CELERY_TASK_QUEUES = (
+    Queue(
+        name="task-collect-coin-quotation",
+        exchange=Exchange("task-collect-coin-quotation", type="direct"),
+        routing_key="task-collect-coin-quotation",
+    ),
+)
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -125,7 +221,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/app/static/"
+STATIC_ROOT = "./static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- backend/realtimequote/realtimequote/urls.py
+++ backend/realtimequote/realtimequote/urls.py
@@ -14,8 +14,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 
+from django.urls import include, path
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
+                                   SpectacularSwaggerView)
+from rest_framework import routers
+
+from realtimequote import settings
+from coin import views as coin_router
+from quotation import views as quotation_router
+
+router = routers.DefaultRouter()
+router.register("coin", coin_router.CoinView)
+router.register("quotation", quotation_router.QuotationViewSet)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("api/", include(router.urls)),
+    path('api/admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += [
+        # swagger patterns
+        path("api/docs/schema/", SpectacularAPIView.as_view(), name="schema"),
+        # Optional UI:
+        path(
+            "api/docs/schema/swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/docs/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+        ),
+        
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
