1d9e4775e1a936bdef73f484a00f1704e5f64650
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-18 15:22:02 -0300

    Created Quotation view

--- backend/realtimequote/quotation/views.py
+++ backend/realtimequote/quotation/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+from middlewares.authentication import JWTAuthentication
+from quotation.serializers import QuotationSerializer
+from quotation.models import Quotation
 
-# Create your views here.
+
+class QuotationViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Quotation.objects.all().order_by("create_date")
+    serializer_class = QuotationSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["coin", "create_date"]
