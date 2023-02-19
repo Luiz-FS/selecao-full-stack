bad1005c4583952804e9f39f48db9be59b0caba0
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-18 21:23:21 -0300

    Add new contraint on Coin model

--- api/realtimequote/apps/coin/models.py
+++ api/realtimequote/apps/coin/models.py
@@ -6,4 +6,9 @@ from realtimequote.base_model import BaseModel
 class Coin(BaseModel):
     name = models.CharField(max_length=30, unique=True, null=False, blank=False)
     description = models.CharField(max_length=250)
-    price = models.DecimalField(max_digits=10 ,decimal_places=2, null=False, blank=False)
+    price = models.DecimalField(max_digits=10 ,decimal_places=4, null=False, blank=False)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(name="unique_name", fields=['name'])
+        ]
