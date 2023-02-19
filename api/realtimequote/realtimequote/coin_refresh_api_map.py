3b23cae7b279d6bfec1a66a68dbba1093c28354b
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-19 11:59:35 -0300

    Created coin refresh api backand map

--- api/realtimequote/realtimequote/coin_refresh_api_map.py
+++ api/realtimequote/realtimequote/coin_refresh_api_map.py
@@ -4,6 +4,6 @@ from backends.kraken_quote_refresh_api import KrakenQuoteRefresh
 
 COIN_TO_BACKEND_API = {
     "BRL / USD": AwesomeapiQuoteRefresh(name="BRL / USD", key="USD-BRL", data_key="USDBRL"),
-    "BTC / EUR": KrakenQuoteRefresh(name="BTC / EUR", key="XBTeur", data_key="XXBTZEUR"),
-    "BTC / USD": KrakenQuoteRefresh(name="BTC / USD", key="XBTusd", data_key="XXBTZUSD"),
+    "BTC / EUR": KrakenQuoteRefresh(name="BTC / EUR", key="XBTeur", data_key="XXBTZEUR", awesomeapi_coin_key="BTC-EUR"),
+    "BTC / USD": KrakenQuoteRefresh(name="BTC / USD", key="XBTusd", data_key="XXBTZUSD", awesomeapi_coin_key="BTC-USD"),
 }
