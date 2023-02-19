be25f96931fd6c7d69053a982e8b882aba7b98b5
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-19 11:44:25 -0300

    Created method to get coin by name

--- api/realtimequote/apps/coin/repository.py
+++ api/realtimequote/apps/coin/repository.py
@@ -3,5 +3,10 @@ from apps.coin.schemas import CoinSchema
 
 
 class CoinRepository:
+    @staticmethod
     def update_current_quote(current_quote: CoinSchema) -> None:
         Coin.objects.filter(name=current_quote.name).update(price=current_quote.price)
+
+    @staticmethod
+    def get_coin_by_name(name: str) -> Coin:
+        return Coin.objects.filter(name=name).first()
