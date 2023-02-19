cf3038dc08399abf18c76b0037a3fd017f7368b2
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-19 12:00:13 -0300

    Created admin page

--- api/realtimequote/apps/coin/admin.py
+++ api/realtimequote/apps/coin/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.coin.models import Coin
 
-# Register your models here.
+@admin.register(Coin)
+class CoinAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'created_at']
+    list_filter = ['name', 'id']
+    ordering = ['name', 'price', 'created_at']
--- api/realtimequote/apps/quotation/admin.py
+++ api/realtimequote/apps/quotation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.quotation.models import Quotation
 
-# Register your models here.
+@admin.register(Quotation)
+class QuotationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'coin', 'max_price', 'min_price', 'created_at']
+    list_filter = ['coin', 'id']
+    ordering = ['max_price', 'min_price', 'created_at']
