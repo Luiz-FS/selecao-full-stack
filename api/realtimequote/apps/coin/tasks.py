a783784a2f065324027231baa6b56d321221300c
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-18 21:26:45 -0300

    Created task to collect current coinquote

--- api/realtimequote/apps/coin/tasks.py
+++ api/realtimequote/apps/coin/tasks.py
@@ -1,5 +1,12 @@
-from realtimequote.celery import app
 from celery import Task
+from realtimequote.celery import app
+from backends.awesomeapi_quote_refresh_api import AwesomeapiQuoteRefresh
+from apps.coin.repository import CoinRepository
+
+
+COIN_TO_BACKEND_API = {
+    "BRL / USD": AwesomeapiQuoteRefresh(name="BRL / USD", key="USD-BRL", data_key="USDBRL")
+}
 
 
 @app.task(
@@ -8,4 +15,9 @@ from celery import Task
     max_retries=20,
 )
 def collect_coin_quotation(self: Task) -> None:
-    print(f"Request {self.request!r}")
+
+    for (coin_name, backend) in COIN_TO_BACKEND_API.items():
+        print(f"Collecting {coin_name}...")
+        current_quote = backend.get_current_quote()
+        CoinRepository.update_current_quote(current_quote)
+        print(current_quote)
