da03677b9f4c0e7826cea104bb3f90dace353b61
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-19 12:00:50 -0300

    Fix decimal fields in model

--- api/realtimequote/apps/coin/models.py
+++ api/realtimequote/apps/coin/models.py
@@ -6,7 +6,10 @@ from realtimequote.base_model import BaseModel
 class Coin(BaseModel):
     name = models.CharField(max_length=30, unique=True, null=False, blank=False)
     description = models.CharField(max_length=250)
-    price = models.DecimalField(max_digits=10 ,decimal_places=4, null=False, blank=False)
+    price = models.DecimalField(max_digits=16 ,decimal_places=4, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         constraints = [
--- api/realtimequote/apps/quotation/models.py
+++ api/realtimequote/apps/quotation/models.py
@@ -4,7 +4,7 @@ from apps.coin.models import Coin
 
 class Quotation(BaseModel):
     coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
-    min_price = models.DecimalField(max_digits=8, decimal_places=4, null=False, blank=False)
-    max_price = models.DecimalField(max_digits=8, decimal_places=4, null=False, blank=False)
-    variance = models.IntegerField(null=False, blank=False)
+    min_price = models.DecimalField(max_digits=16, decimal_places=4, null=False, blank=False)
+    max_price = models.DecimalField(max_digits=16, decimal_places=4, null=False, blank=False)
+    variance = models.DecimalField(max_digits=4, decimal_places=2, null=False, blank=False)
     create_date = models.DateField(null=False, blank=False)
