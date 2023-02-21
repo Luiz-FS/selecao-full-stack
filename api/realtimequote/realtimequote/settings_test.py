2df7f1e538807056001322a5e38313fd6d59ba5d
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-20 23:05:42 -0300

    Created test settings

---
+++ api/realtimequote/realtimequote/settings_test.py
@@ -0,0 +1,13 @@
+from realtimequote.settings import *
+
+DEBUG = True
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+# Celery
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_BROKER_URL = config("CELERY_BROKER_URL", "memory://")
