89e408f9ff4142189ce4dad2bcd125a9af86a3a6
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2023-02-23 11:16:39 -0300

    Created root layout

---
+++ frontend/src/layouts/Root.js
@@ -0,0 +1,26 @@
+import React, { Suspense } from 'react';
+import { Outlet } from "react-router-dom";
+import { LinearProgress } from '@mui/material';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import NavBar from './navbar';
+
+const Root = props => {
+    return (
+        <Box sx={{ display: 'flex', backgroundColor: "rgba(236, 246, 255, 0.3);" , minHeight: "100vh"}}>
+            <CssBaseline />
+            <NavBar></NavBar>
+            <Box component="main" sx={{ flexGrow: 1, p: 3, padding: 0 }}>
+                <Toolbar />
+                <Suspense fallback={<LinearProgress />}>
+                    <Outlet />
+                </Suspense>
+            </Box>
+        </Box>
+    )
+}
+
+
+
+export default Root;
